977ac587afbcbb3b44b16b6c6758e41ee4798196
jerholl <jeremy_hollenbeck@hotmail.com>
2026-02-28 18:27:57 -0600

    all

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from "sanity";
 import {deskTool} from "sanity/desk";
 import {schemaTypes} from "./schemaTypes";
 
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID || "replace-me";
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID || "xeqvf5vl";
 const dataset = process.env.SANITY_STUDIO_DATASET || "production";
 
 export default defineConfig({
